bef33444743c27c5c6427b1f40a38ee529ae7110
Anna Baburina <4nna.baburina@gmail.com>
2021-06-06 08:44:31 +0500

    ADD solution: largest palindrome product (recursive)

--- largest_palindrome_product.rb
+++ largest_palindrome_product.rb
@@ -1,25 +1,42 @@
 # frozen_string_literal: true
 
 # Iterative solution
-def lpp
-  first_factor = 999
-  second_factor = 999
-  array_of_palindromes = []
+# def lpp
+#   first_factor = 999
+#   second_factor = 999
+#   array_of_palindromes = []
 
-  while first_factor > 1
-    array_of_palindromes.push(first_factor * second_factor) if palindrome(first_factor * second_factor)
-    first_factor -= 1
-    while second_factor > 1
-      array_of_palindromes.push(first_factor * second_factor) if palindrome(first_factor * second_factor)
-      second_factor -= 1
-    end
-    second_factor = 999
-  end
-  array_of_palindromes.max
-end
+#   while first_factor > 1
+#     array_of_palindromes.push(first_factor * second_factor) if palindrome(first_factor * second_factor)
+#     first_factor -= 1
+#     while second_factor > 1
+#       array_of_palindromes.push(first_factor * second_factor) if palindrome(first_factor * second_factor)
+#       second_factor -= 1
+#     end
+#     second_factor = 999
+#   end
+#   array_of_palindromes.max
+# end
+
+# p lpp
 
 def palindrome(product)
   product.to_s == product.to_s.reverse
 end
 
-p lpp
+def lowering_first_factor(first_factor = 999, second_factor = 999, largest = [])
+  return largest.max if first_factor == 1
+
+  largest.push(first_factor * second_factor) if palindrome(first_factor * second_factor)
+  lowering_second_factor(first_factor, second_factor, largest)
+  lowering_first_factor(first_factor -= 1, second_factor, largest)
+end
+
+def lowering_second_factor(first_factor, second_factor, largest)
+  return largest.max if second_factor == 1
+
+  largest.push(first_factor * second_factor) if palindrome(first_factor * second_factor)
+  lowering_second_factor(first_factor, second_factor -= 1, largest)
+end
+
+p lowering_first_factor
